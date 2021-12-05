384ca2b1ad8a03b51958c6246315b2e9e807ae76
Ayfri <pierre.ayfri@gmail.com>
2021-12-05 16:00:52 +0100

    feat: Add day one.

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,3 +1,11 @@
 fun main() {
-
+	val data = readFile("day1.txt")
+	val pairs = data.trimEnd().split("\n").zipWithNext().toMap()
+	val result = pairs.count { a ->
+		return@count a.key == a.value
+	}
+	
+	pairs.forEach { println("key=${it.key}, value=${it.value}") }
+	
+	println(result)
 }
